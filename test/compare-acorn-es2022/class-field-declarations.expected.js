81d0c456b8150bf1c675b50c23fc4bd858abc936
Brook <uedsky@gmail.com>
2024-07-17 09:35:44 +0800

    test: class fields

---
+++ test/compare-acorn-es2022/class-field-declarations.expected.js
@@ -0,0 +1,10 @@
+class FieldDeclarations {
+    noValue;
+    member = 1;
+    ['non-identifier'] = 18;
+    [Symbol.iterator] = 10;
+    #privateMember = 2;
+    static #STATIC = 3;
+    static STATIC = 4;
+    static [1 + 2] = 4;
+}
